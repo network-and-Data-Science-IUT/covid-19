1e632a710abb84fdead5fa10342e404d557dbee0
arezooh <48172387+arezooh@users.noreply.github.com>
2021-01-11 00:45:38 +0330

    Add assessing codes

---
+++ prediction codes/assessions/gap_30%_val/5/sc.py
@@ -0,0 +1,22 @@
+from sys import argv
+import sys
+import os
+import subprocess
+from pexecute.process import ProcessLoom
+
+r = 5
+
+def Run(i):
+    subprocess.call("python ./prediction.py "+str(i), shell=True)
+
+def main():
+    for i in range(min((2 * (10 - r)) + 1, 7)):
+        print(200 * '*')
+        print(i)
+        Run(i)
+
+
+
+if __name__ == "__main__":
+
+    main()
---
+++ prediction codes/assessions/gap_7_test_7_val/death/Readme.txt
@@ -0,0 +1,1 @@
+In each of the folders, 1 to 6 prediction.py must be executed without any argument as follows: python prediction.py
